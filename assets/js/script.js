524bbffd84521dec9732f365985f4426023a410e
hebertluisofc <hebert.goncalves@ba.estudante.senai.br>
2025-10-10 21:07:11 -0300

    login

--- assets/js/script.js
+++ assets/js/script.js
@@ -37,13 +37,18 @@
 // next.addEventListener('click' , nextSlider);
 // back.addEventListener('click'   , backSlider);
 
+function forgotPassword() {
+    alert("Usuário: admin\nSenha: password");
+}
+
 function login() {
+
     var username = document.getElementById("username").value;
     var password = document.getElementById("password").value;
+    
     if (username === "admin" && password === "password") {
-        alert("Login successful!");
-        window.location.href = "index.html"; // Redirect to dashboard or another page
+        window.location.href = "mainPage.html"; // Redirect to dashboard or another page
     } else {
-        alert("Invalid username or password.");
+        alert("Usuário ou senha incorretos. Tente novamente!");
     }
 }
